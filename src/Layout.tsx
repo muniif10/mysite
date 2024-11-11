c63c4dc42a15b7ec6ba996c96ef3bfe5a669a67d
Muniif <muniif10@gmail.com>
2024-11-11 19:53:23 +0800

    missed an unused snippet

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useState } from "react";
 import SideNav from "./components/sidenav";
 import navLinks from "@/navbar_routes";
 import { useLocation } from "react-router-dom";
